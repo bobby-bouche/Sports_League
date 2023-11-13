90e29ac503abbf229fb5a3568a6fac7026212c41
bobby-bouche <n_8-bit_bobby@hotmail.com>
2023-11-13 10:49:14 -0800

    add team fields

--- src/data_classes/Team.java
+++ src/data_classes/Team.java
@@ -3,19 +3,44 @@ package data_classes;
 
 import java.util.Map;
 
+
 public class Team {
 	
 	// team fields
 	private Map<String, Player> squad;
-	
+
+	private String name;
+	private String location;
 	private int gamesPlayed;
 	private int points;
 	private int goalsFor;
 	private int goalsAgainst;
 	
 	
-	// constructors
+	// symbolic constants
+	private static int DEFAULT_GAMES_PLAYED = 0;
+	private static int DEFAULT_POINTS 		= 0;
+	private static int DEFAULT_GOALSFOR 	= 0;
+	private static int DEFAULT_GOALSAGAINST = 0;
 	
+	
+	// initializer
+	{
+		gamesPlayed  = DEFAULT_GAMES_PLAYED;
+		points       = DEFAULT_POINTS;
+		goalsFor     = DEFAULT_GOALSFOR;
+		goalsAgainst = DEFAULT_GOALSAGAINST;
+	}
+	
+	
+	// constructors
+	public Team() {
+		super();
+	}
+	
+	public Team() {
+		
+	}
 	
 
 }
