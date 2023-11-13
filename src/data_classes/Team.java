2cc74ffbb6b9905a9f91cb47a809a7ff4623b4e4
bobby-bouche <n_8-bit_bobby@hotmail.com>
2023-11-12 21:11:33 -0800

    added team class. Also added player match rating to player class

---
+++ src/data_classes/Team.java
@@ -0,0 +1,20 @@
+package data_classes;
+
+import java.util.ArrayList;
+
+public class Team {
+	
+	// team fields
+	private ArrayList<Player> squad;
+	
+	private int gamesPlayed;
+	private int points;
+	private int goalsFor;
+	private int goalsAgainst;
+	
+	
+	// constructors
+	
+	
+
+}
